8b200d2255375ccd84b868d203918abda57453e5
Jingxian <45543558+JingxianM@users.noreply.github.com>
2022-07-31 22:08:53 +0200

    Update instructions on GUI

---
+++ FixCSVAssignment/FixCSVAssignment/MainForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace FixCSVAssignment
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.EditCellButton = new System.Windows.Forms.Button();
+            this.ExportFileButton = new System.Windows.Forms.Button();
+            this.DeleteRowButton = new System.Windows.Forms.Button();
+            this.DeleteRowUpDown = new System.Windows.Forms.NumericUpDown();
+            this.DeleteColumnButton = new System.Windows.Forms.Button();
+            this.DeleteColumnUpDown = new System.Windows.Forms.NumericUpDown();
+            this.AddColumnButton = new System.Windows.Forms.Button();
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.LoadFileButton = new System.Windows.Forms.Button();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.ErrorsChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ChartViewButton = new System.Windows.Forms.Button();
+            this.TableViewButton = new System.Windows.Forms.Button();
+            this.RowColLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DeleteRowUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DeleteColumnUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorsChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EditCellButton
+            // 
+            this.EditCellButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.EditCellButton.FlatAppearance.BorderSize = 0;
+            this.EditCellButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EditCellButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditCellButton.ForeColor = System.Drawing.Color.White;
+            this.EditCellButton.Location = new System.Drawing.Point(576, 592);
+            this.EditCellButton.Name = "EditCellButton";
+            this.EditCellButton.Size = new System.Drawing.Size(110, 51);
+            this.EditCellButton.TabIndex = 22;
+            this.EditCellButton.Text = "Edit Cell";
+            this.EditCellButton.UseVisualStyleBackColor = false;
+            // 
+            // ExportFileButton
+            // 
+            this.ExportFileButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ExportFileButton.FlatAppearance.BorderSize = 0;
+            this.ExportFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExportFileButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExportFileButton.ForeColor = System.Drawing.Color.White;
+            this.ExportFileButton.Location = new System.Drawing.Point(448, 592);
+            this.ExportFileButton.Name = "ExportFileButton";
+            this.ExportFileButton.Size = new System.Drawing.Size(110, 51);
+            this.ExportFileButton.TabIndex = 21;
+            this.ExportFileButton.Text = "Export File";
+            this.ExportFileButton.UseVisualStyleBackColor = false;
+            // 
+            // DeleteRowButton
+            // 
+            this.DeleteRowButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.DeleteRowButton.FlatAppearance.BorderSize = 0;
+            this.DeleteRowButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteRowButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteRowButton.ForeColor = System.Drawing.Color.White;
+            this.DeleteRowButton.Location = new System.Drawing.Point(926, 592);
+            this.DeleteRowButton.Name = "DeleteRowButton";
+            this.DeleteRowButton.Size = new System.Drawing.Size(124, 51);
+            this.DeleteRowButton.TabIndex = 20;
+            this.DeleteRowButton.Text = "Delete Row";
+            this.DeleteRowButton.UseVisualStyleBackColor = false;
+            // 
+            // DeleteRowUpDown
+            // 
+            this.DeleteRowUpDown.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteRowUpDown.Location = new System.Drawing.Point(1056, 599);
+            this.DeleteRowUpDown.Name = "DeleteRowUpDown";
+            this.DeleteRowUpDown.Size = new System.Drawing.Size(60, 36);
+            this.DeleteRowUpDown.TabIndex = 19;
+            // 
+            // DeleteColumnButton
+            // 
+            this.DeleteColumnButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.DeleteColumnButton.FlatAppearance.BorderSize = 0;
+            this.DeleteColumnButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteColumnButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteColumnButton.ForeColor = System.Drawing.Color.White;
+            this.DeleteColumnButton.Location = new System.Drawing.Point(704, 592);
+            this.DeleteColumnButton.Name = "DeleteColumnButton";
+            this.DeleteColumnButton.Size = new System.Drawing.Size(150, 51);
+            this.DeleteColumnButton.TabIndex = 18;
+            this.DeleteColumnButton.Text = "Delete Column";
+            this.DeleteColumnButton.UseVisualStyleBackColor = false;
+            // 
+            // DeleteColumnUpDown
+            // 
+            this.DeleteColumnUpDown.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteColumnUpDown.Location = new System.Drawing.Point(860, 599);
+            this.DeleteColumnUpDown.Name = "DeleteColumnUpDown";
+            this.DeleteColumnUpDown.Size = new System.Drawing.Size(60, 36);
+            this.DeleteColumnUpDown.TabIndex = 17;
+            // 
+            // AddColumnButton
+            // 
+            this.AddColumnButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.AddColumnButton.FlatAppearance.BorderSize = 0;
+            this.AddColumnButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddColumnButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddColumnButton.ForeColor = System.Drawing.Color.White;
+            this.AddColumnButton.Location = new System.Drawing.Point(16, 592);
+            this.AddColumnButton.Name = "AddColumnButton";
+            this.AddColumnButton.Size = new System.Drawing.Size(177, 51);
+            this.AddColumnButton.TabIndex = 16;
+            this.AddColumnButton.Text = "Add Column";
+            this.AddColumnButton.UseVisualStyleBackColor = false;
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.AddRowButton.FlatAppearance.BorderSize = 0;
+            this.AddRowButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddRowButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddRowButton.ForeColor = System.Drawing.Color.White;
+            this.AddRowButton.Location = new System.Drawing.Point(199, 592);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(103, 51);
+            this.AddRowButton.TabIndex = 15;
+            this.AddRowButton.Text = "Add Row";
+            this.AddRowButton.UseVisualStyleBackColor = false;
+            // 
+            // LoadFileButton
+            // 
+            this.LoadFileButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.LoadFileButton.FlatAppearance.BorderSize = 0;
+            this.LoadFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoadFileButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadFileButton.ForeColor = System.Drawing.Color.White;
+            this.LoadFileButton.Location = new System.Drawing.Point(332, 592);
+            this.LoadFileButton.Name = "LoadFileButton";
+            this.LoadFileButton.Size = new System.Drawing.Size(110, 51);
+            this.LoadFileButton.TabIndex = 14;
+            this.LoadFileButton.Text = "Load File";
+            this.LoadFileButton.UseVisualStyleBackColor = false;
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.AllowUserToAddRows = false;
+            this.DataGridView.AllowUserToDeleteRows = false;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(90, 69);
+            this.DataGridView.MultiSelect = false;
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.ReadOnly = true;
+            this.DataGridView.Size = new System.Drawing.Size(947, 508);
+            this.DataGridView.TabIndex = 23;
+            // 
+            // ErrorsChart
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.ErrorsChart.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.ErrorsChart.Legends.Add(legend6);
+            this.ErrorsChart.Location = new System.Drawing.Point(336, 74);
+            this.ErrorsChart.Name = "ErrorsChart";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.ErrorsChart.Series.Add(series6);
+            this.ErrorsChart.Size = new System.Drawing.Size(500, 500);
+            this.ErrorsChart.TabIndex = 24;
+            this.ErrorsChart.Text = "Errors";
+            // 
+            // ChartViewButton
+            // 
+            this.ChartViewButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ChartViewButton.FlatAppearance.BorderSize = 0;
+            this.ChartViewButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ChartViewButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChartViewButton.ForeColor = System.Drawing.Color.White;
+            this.ChartViewButton.Location = new System.Drawing.Point(576, 12);
+            this.ChartViewButton.Name = "ChartViewButton";
+            this.ChartViewButton.Size = new System.Drawing.Size(153, 38);
+            this.ChartViewButton.TabIndex = 26;
+            this.ChartViewButton.Text = "Chart View";
+            this.ChartViewButton.UseVisualStyleBackColor = false;
+            this.ChartViewButton.Click += new System.EventHandler(this.ChartViewButton_Click);
+            // 
+            // TableViewButton
+            // 
+            this.TableViewButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.TableViewButton.FlatAppearance.BorderSize = 0;
+            this.TableViewButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TableViewButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TableViewButton.ForeColor = System.Drawing.Color.White;
+            this.TableViewButton.Location = new System.Drawing.Point(417, 12);
+            this.TableViewButton.Name = "TableViewButton";
+            this.TableViewButton.Size = new System.Drawing.Size(153, 38);
+            this.TableViewButton.TabIndex = 25;
+            this.TableViewButton.Text = "Table View";
+            this.TableViewButton.UseVisualStyleBackColor = false;
+            this.TableViewButton.Click += new System.EventHandler(this.TableViewButton_Click);
+            // 
+            // RowColLabel
+            // 
+            this.RowColLabel.AutoSize = true;
+            this.RowColLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RowColLabel.Location = new System.Drawing.Point(86, 42);
+            this.RowColLabel.Name = "RowColLabel";
+            this.RowColLabel.Size = new System.Drawing.Size(91, 24);
+            this.RowColLabel.TabIndex = 27;
+            this.RowColLabel.Text = "Row: Col:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 655);
+            this.Controls.Add(this.RowColLabel);
+            this.Controls.Add(this.ChartViewButton);
+            this.Controls.Add(this.TableViewButton);
+            this.Controls.Add(this.ErrorsChart);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.EditCellButton);
+            this.Controls.Add(this.ExportFileButton);
+            this.Controls.Add(this.DeleteRowButton);
+            this.Controls.Add(this.DeleteRowUpDown);
+            this.Controls.Add(this.DeleteColumnButton);
+            this.Controls.Add(this.DeleteColumnUpDown);
+            this.Controls.Add(this.AddColumnButton);
+            this.Controls.Add(this.AddRowButton);
+            this.Controls.Add(this.LoadFileButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DeleteRowUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DeleteColumnUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorsChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EditCellButton;
+        private System.Windows.Forms.Button ExportFileButton;
+        private System.Windows.Forms.Button DeleteRowButton;
+        private System.Windows.Forms.NumericUpDown DeleteRowUpDown;
+        private System.Windows.Forms.Button DeleteColumnButton;
+        private System.Windows.Forms.NumericUpDown DeleteColumnUpDown;
+        private System.Windows.Forms.Button AddColumnButton;
+        private System.Windows.Forms.Button AddRowButton;
+        private System.Windows.Forms.Button LoadFileButton;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ErrorsChart;
+        private System.Windows.Forms.Button ChartViewButton;
+        private System.Windows.Forms.Button TableViewButton;
+        private System.Windows.Forms.Label RowColLabel;
+    }
+}
+
---
+++ FixCSVAssignment/FixCSVAssignment/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixCSVAssignment
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ErrorsChart.Visible = false;
+        }
+
+        private void TableViewButton_Click(object sender, EventArgs e)
+        {
+            ErrorsChart.Visible = false;
+            DataGridView.Visible = true;
+        }
+
+        private void ChartViewButton_Click(object sender, EventArgs e)
+        {
+            ErrorsChart.Visible = true;
+            DataGridView.Visible = false;
+        }
+    }
+}
